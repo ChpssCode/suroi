02aa05c8e8089eaac43600b1f334e638fabe6fb2
ChpssCode <paaxyman@gmail.com>
2024-06-16 18:49:49 +0000

    initial commit

--- client/src/scripts/config.ts
+++ client/src/scripts/config.ts
@@ -5,28 +5,13 @@ export const Config = {
             mainAddress: "http://127.0.0.1:8000",
             gameAddress: "ws://127.0.0.1:800<ID>"
         },
-        na: {
-            name: "North America",
-            mainAddress: "https://na.suroi.io",
-            gameAddress: "wss://<ID>.na.suroi.io"
+        clanwar: {
+            name: "4v4",
+            mainAddress: "http://82.165.212.40",
+            gameAddress: "ws://82.165.212.40:800<ID>"
         },
-        eu: {
-            name: "Europe",
-            mainAddress: "https://eu.suroi.io",
-            gameAddress: "wss://<ID>.eu.suroi.io"
-        },
-        sa: {
-            name: "South America",
-            mainAddress: "https://sa.suroi.io",
-            gameAddress: "wss://<ID>.sa.suroi.io"
-        },
-        as: {
-            name: "Asia",
-            mainAddress: "https://as.suroi.io",
-            gameAddress: "wss://<ID>.as.suroi.io"
-        }
     },
-    defaultRegion: "na",
+    defaultRegion: "clanwar",
     mode: "normal"
 } satisfies ConfigType as ConfigType;
 
